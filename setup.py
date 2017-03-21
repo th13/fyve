844ee8924fd313dd39d917e0f10a256167679f41
Trevor Helms <thelms501@gmail.com>
2017-03-21 09:44:19 -0400

    Init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup 
+
+setup(name='fyve',
+      version='1.0',
+      description='A library to overcomplicated five--the Python way.',
+      url='http://github.com/th14/fyve',
+      author='Trevor Helms',
+      author_email='me@trevorhelms.com',
+      license='MIT',
+      packages=['fyve'],
+      zip_safe=False)
